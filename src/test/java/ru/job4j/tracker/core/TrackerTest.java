176c9f370e574f0ecd3cfb52488fcd0517eaa6dd
Denis.Kitrish <denis.kitrish@outlook.com>
2024-08-05 15:06:42 +0300

    Initial structure of program

---
+++ src/test/java/ru/job4j/tracker/core/TrackerTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TrackerTest {
+	@Test
+	public void whenAddNewItemThenTrackerHasSameItem() {
+		Tracker tracker = new Tracker();
+		Item item = new Item();
+		item.setName("test1");
+		tracker.add(item);
+		Item result = tracker.findById(item.getId());
+		assertThat(result.getName()).isEqualTo(item.getName());
+	}
+}
