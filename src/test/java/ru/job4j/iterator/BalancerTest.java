c4eb41f1e09be26393918651060ea4191fdba0f0
Denis.Kitrish <denis.kitrish@outlook.com>
2024-11-27 20:28:03 +0300

    7. Балансир [#505083]

--- src/test/java/ru/job4j/iterator/BalancerTest.java
+++ src/test/java/ru/job4j/iterator/BalancerTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BalancerTest {
 	@Test
